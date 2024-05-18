551384e4629364f328198a8147e929fee75f9a5b
Saul Johnson <saul.a.johnson@gmail.com>
2024-05-18 09:47:35 +0200

    Add poison generator

---
+++ genpoison.py
@@ -0,0 +1,33 @@
+import sys
+from PIL import Image, ImageDraw
+from random import randint
+
+
+DIVISIONS = 3
+""" The number of divisions to divide the image into.
+"""
+
+
+# Check args and print usage.
+if len(sys.argv) < 3:
+    print('Usage: python genpoison.py <scale_factor> <output_file>')
+
+
+# Initialize image and drawing object.
+scale = int(sys.argv[1])
+img = Image.new('RGB', (scale * DIVISIONS, scale * DIVISIONS), color=(255, 255, 255))
+draw = ImageDraw.Draw(img)
+
+
+# For each grid cell...
+for x in range(0, DIVISIONS):
+    for y in range(0, DIVISIONS):
+        flip = randint(0, 1) # Flip a coin.
+
+        # If heads, draw black rectangle.
+        if flip == 1:
+            draw.rectangle([(x * scale, y * scale), ((x + 1) * scale, (y + 1) * scale)], (0, 0, 0))
+
+
+# Save output.
+img.save(sys.argv[2], "PNG")
